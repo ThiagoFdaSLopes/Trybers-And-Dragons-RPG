58bab8473b6b06459a7749e8ba16c8eba4cf351d
Thiago <programadorthiagolopes@gmail.com>
2023-02-16 16:27:58 -0300

    feat: extends Interface SimpleFighter Liskov

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -5,6 +5,6 @@ export default interface Fighter extends SimpleFighter {
   defense: number,
   energy?: Energy
 
-  special?(enemy: Fighter): void
+  special?(enemy: SimpleFighter): void
   levelUp(): void
 }
