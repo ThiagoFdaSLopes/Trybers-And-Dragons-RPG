17814b232c2cdcbbc53a3081e0aebef9eab92170
Thiago <programadorthiagolopes@gmail.com>
2023-02-16 14:38:20 -0300

    feat: Barrel atualizado

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,8 @@
 import Race from './Race';
+import Dwarf from './Dwarf';
+import Orc from './Orc';
+import Elf from './Elf';
+import Halfling from './Halfling';
 
 export default Race;
+export { Dwarf, Orc, Elf, Halfling };
