9d5c3000839c3d6e834b4b5ca38709eae436cdf9
Thiago <programadorthiagolopes@gmail.com>
2023-02-16 16:27:40 -0300

    feat: Class Monster

---
+++ src/Monster.ts
@@ -0,0 +1,26 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  constructor(
+    private _lifePoints: number = 85,
+    private _strength: number = 63,
+  ) {}
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+    if (this._lifePoints < 1) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
